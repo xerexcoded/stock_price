6ede4c249470a35edac745e7b37ad77aabf9760a
xerexcoded <arnavbanerjee61@gmail.com>
2021-09-13 12:01:39 +0530

    half way

--- stock_price.sh
+++ stock_price.sh
@@ -1,2 +1,54 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
+set -e 
+LANG =C 
+LC_NUMERIC =C
+SYMBOLS=("$0")
+
+if ! $(type jq > /dev/null 2>&1); then
+  echo "'jq is not in PATH."
+  exit 1
+fi
+
+if [-z "$SYMBOLS"]; then
+  echo "Usage : ./ticker.sh GOOG BTC-USD"
+  exit
+fi
+
+FIELDS=(symbol marketState regularMarketPrice regularMarketChange regularMarketChangePercent \
+  preMarketPrice preMarketChange preMarketChangePercent postMarketPrice postMarketChange postMarketChangePercent)
+API_ENDPOINT="https://query1.finance.yahoo.com/v7/finance/quote?lang=en-US&region=US&corsDomain=finance.yahoo.com"
+
+if [-z "$NO_COLOR"]; then
+  : "${COLOR_BOLD:=\e[1;37m}"
+  : "${COLOT_GREEN:=\e[32m}"
+  : "${COLOR_RED:=\e[31m}"
+  : "${COLOR_RESET:=\e[00m}"
+fi
+
+symbols= $(IFS=,; echo "${SYMBOLS[*]}")
+fields= $(IFS=,; echo "${FIELDS[*]}")
+
+results=$(curl --silent "$API_ENDPOINT&fields=$fields&symbols=$symbols" \ 
+| jq '.quoteResponse .result')
+
+query () {
+  echo $results | jq -r ".[] | select(.symbol== \"$1\") | .$2"
+}
+
+for symbol in $(IFS=' '; echo "${SYMBOLS[*]}" | tr '[:lower:]' '[:upper:]'); do
+  marketState="$(query $symbol 'marketState')"
+
+  if [-z $marketState]; then
+    printf 'No result for symbol "%s"\n' $symbol
+    continue
+  fi
+
+  preMarketChange="$(query $symbol 'preMarketChange')"
+  postMarketChange="$(query $symbol 'postMarketChange')"
+
+  if [$marketState == "PRE"] \
+    && [$preMarketChange != "0"] \
+    && [$preMarketChange != "null"]; then
+  nonRegularMarketSugn='*'
+  c
